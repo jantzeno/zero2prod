493c418101eec999f948d04a327154d72672b961
Jantzen Owens <jantzeno@msn.com>
2023-04-03 08:39:47 -0400

    Split into sub-modules
    Chapter 3.8.5.2.1

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -22,7 +22,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind address");
     let port = listener.local_addr().unwrap().port();
 
-    let server = zero2prod::run(listener).expect("failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
