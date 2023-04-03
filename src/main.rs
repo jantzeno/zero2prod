4aa6a45f41720a1bde2a493f4332a8233f23cf73
Jantzen Owens <jantzeno@msn.com>
2023-04-03 09:28:53 -0400

    Connect to database and update tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
+    // Panic if configuration cannot be read
+    let configuration = get_configuration().expect("failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
