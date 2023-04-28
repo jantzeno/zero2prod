9a43387aad088044088be8aa139f3d1f47875f8e
Jantzen Owens <jantzeno@msn.com>
2023-04-28 13:59:44 -0400

    Setup inital logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Logging
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if configuration cannot be read
     let configuration = get_configuration().expect("failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
