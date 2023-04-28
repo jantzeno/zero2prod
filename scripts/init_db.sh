ff0e70ff811b6e1d4e38409edcdb1557c620e59c
Jantzen Owens <jantzeno@msn.com>
2023-04-28 12:28:07 -0400

    Fixed typo in db init script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -40,7 +40,7 @@ then
         -e POSTGRES_USER=${DB_USER} \
         -e POSTGRES_PASSWORD=${DB_PASSWORD} \
         -e POSTGRES_DB=${DB_NAME} \
-        -p "${DB_PORT}":5432 \
+        -p "${DB_PORT}":"${DB_PORT}" \
         -d docker.io/library/postgres:latest \
         postgres -N 1000
         # ^ Increased maximum number of connections for testing purposes
