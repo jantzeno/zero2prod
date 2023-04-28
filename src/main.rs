dacc835e9fbdf5928a1c0e95bc2d55785c3b1c3e
Jantzen Owens <jantzeno@msn.com>
2023-04-28 13:31:27 -0400

    End of Chapter 3
    Database tests are ran in isolation.
    Database tests create a random uuid v4 database name.
    Recreate database container to clear test data.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
@@ -5,7 +6,10 @@ use zero2prod::{configuration::get_configuration, startup::run};
 async fn main() -> Result<(), std::io::Error> {
     // Panic if configuration cannot be read
     let configuration = get_configuration().expect("failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("failed to connect to database");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
